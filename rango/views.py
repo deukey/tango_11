737755bfc6f8baf3341ed64156fef1da0051f812
deukey <deukey@naver.com>
2016-09-02 17:00:11 +0900

    Added templates, static files

--- rango/views.py
+++ rango/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Hey there partner!")
+    context_dict = {'boldmessage': "Crunch, creamy, cookie, candy, cupcake!"}
+    return render(request, 'rango/index.html', context=context_dict)
+
+
+def about(request):
+    context_dict = {'boldmessage': "This tutorial has been put together by Hyperdog."}
+    return render(request, 'rango/about.html', context=context_dict)
--- tango_11/settings.py
+++ tango_11/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'tango_11.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tango_11',
+        'USER': 'deukey',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
